c5fab40b67fa8c315ccd864d0a9f747c724f601f
TimGibbs <timgibbs1988@gmail.com>
2022-09-15 13:11:34 +0100

    fix

--- src/pages/weapons/GenericWeapons.tsx
+++ src/pages/weapons/GenericWeapons.tsx
@@ -1,6 +1,6 @@
 import GenericWeaponsList from "../../data/weapons/GenericWeapons";
 import WeaponBlock from "../components/weapons/weaponsBlock/WeaponsBlock";
-import { WeaponTileProps } from "../components/weaponTIle/WeaponTile";
+import { WeaponTileProps } from "../components/weapons/weaponTIle/WeaponTile";
 
 const items :WeaponTileProps[] = GenericWeaponsList.map(o=>({weapon:o}))
 
